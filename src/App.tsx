bafd28a9b0fdfd9314d11f6661d46014ca7f473c
ZonRyu <julyonchang@gmail.com>
2024-10-17 23:10:50 +0700

    Navbar Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Navbar from '@/components/navbar'
+import { useState } from 'react'
+
 function App() {
+  const [selectedPage, setSelectedPage] = useState<string>('home')
+
   return (
-    <div className='app'>
-      
+    <div className='app bg-gray-20'>
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
     </div>
   )
 }
